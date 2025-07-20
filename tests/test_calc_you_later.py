36716e7356e3b38e7058a5a9ff0b01143b5e1019
Aleksandr Nokhrin <a.v.nokhrin@yandex.ru>
2025-07-20 22:37:06 +0500

    Получение значений для калькулятора
    - добавил чтение значений из параметров
    - добавил юнит-тест для cli

--- tests/test_calc_you_later.py
+++ tests/test_calc_you_later.py
@@ -1,5 +1,5 @@
 import pytest
-from utils.calc_you_later import add, mult
+from utils.calc_you_later import add, mult, main
 
 
 def test_add() -> int:
@@ -12,5 +12,7 @@ def test_mult() -> int:
     actual = mult(0, 1)
     assert expected == actual
 
-def test_get_parameters():
-    assert 0
+def test_main(capsys):
+    main(['add', '1', '1', '-v', '2'])
+    captured = capsys.readouterr()
+    assert captured.out == 'Сумма 1 и 1 равна 2\n'
